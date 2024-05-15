9dd17c52b91f1c33c95a815ffafb15bd8dab264a
munoz <cemunoz230101@gmail.com>
2024-05-15 02:39:37 -0700

    rm brewfile, wsl clipboard working and new readme

--- .config/nvim/lua/muñoz/core/options.lua
+++ .config/nvim/lua/muñoz/core/options.lua
@@ -2,13 +2,13 @@ local opt = vim.opt -- for conciseness
 
 -- line numbers
 opt.relativenumber = true -- show relative line numbers
-opt.number = true -- shows absolute line number on cursor line (when relative number is on)
+opt.number = true         -- shows absolute line number on cursor line (when relative number is on)
 
 -- tabs & indentation
 opt.title = true
-opt.tabstop = 4 -- spaces for tabs (prettier default)
-opt.shiftwidth = 4 -- spaces for indent width
-opt.expandtab = true -- expand tab to spaces
+opt.tabstop = 4       -- spaces for tabs (prettier default)
+opt.shiftwidth = 4    -- spaces for indent width
+opt.expandtab = true  -- expand tab to spaces
 opt.autoindent = true -- copy indent from current line when starting new one
 
 -- line wrapping
@@ -16,16 +16,15 @@ opt.wrap = false -- disable line wrapping
 
 -- search settings
 opt.ignorecase = true -- ignore case when searching
-opt.smartcase = true -- if you include mixed case in your search, assumes you want case-sensitive
+opt.smartcase = true  -- if you include mixed case in your search, assumes you want case-sensitive
 
 -- cursor line
 opt.cursorline = true -- highlight the current cursor line
 
 -- backspace
-opt.backspace = "indent,eol,start" -- allow backspace on indent, end of line or insert mode start position
+opt.backspace = "indent,eol,start"  -- allow backspace on indent, end of line or insert mode start position
 
--- clipboard
-opt.clipboard:append("unnamedplus") -- use system clipboard as default register
+opt.clipboard:append("unnamedplus") -- use system clipboard as default registe
 
 -- split windows
 opt.splitright = true -- split vertical window to the right
@@ -34,18 +33,34 @@ opt.splitbelow = true -- split horizontal window to the bottom
 -- turn off swapfile
 opt.swapfile = false
 
-opt.iskeyword:append("-")
+-- mac and wsl system clipboard
+opt.clipboard:append("unnamedplus") -- use system clipboard as default register
 
-opt.scrolloff = 8 -- scroll page when cursor is 8 lines from top/bottom
+if vim.fn.has('wsl') == 1 then
+    vim.g.clipboard = {
+        name = 'WslClipboard',
+        copy = {
+            ['+'] = 'clip.exe',
+            ['*'] = 'clip.exe',
+        },
+        paste = {
+            ['+'] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+            ['*'] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+        },
+        cache_enabled = 0,
+    }
+end
+
+opt.scrolloff = 8         -- scroll page when cursor is 8 lines from top/bottom
 vim.opt.sidescrolloff = 8 -- scroll page when cursor is 8 spaces from left/right
 
 opt.termguicolors = true
 
 -- Highlight when yanking (copying) text
 vim.api.nvim_create_autocmd("TextYankPost", {
-	desc = "Highlight when yanking (copying) text",
-	group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
-	callback = function()
-		vim.highlight.on_yank()
-	end,
+    desc = "Highlight when yanking (copying) text",
+    group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+    callback = function()
+        vim.highlight.on_yank()
+    end,
 })
--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# write the read me file
+# this are my wsl\mac dotfiles for:
+
+- zsh
+- neovim
+- tmux
+
+and some snippets!
+
+## todos:
+
+- improve readme 
+- automate everything with a bash script
