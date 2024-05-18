edb132715dd287e439e271f252008659758fddab
munoz <cemunoz230101@gmail.com>
2024-05-18 14:49:41 -0700

    ensured installed

--- .config/nvim/lua/muñoz/plugins/some-plugins.lua
+++ .config/nvim/lua/muñoz/plugins/some-plugins.lua
@@ -1,6 +1,12 @@
 return {
 	{
 		"christoomey/vim-tmux-navigator", -- tmux & split window navigation
+	},
+	{
 		"numToStr/Comment.nvim", -- comment line(s) with gc in visual mode and gcc in normal mode
+		opts = {
+			-- add any options here
+		},
+		lazy = false,
 	},
 }
