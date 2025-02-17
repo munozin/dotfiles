e92fae9996ec5773146914ecd5d533cd729bd1cc
munozin <cemunoz230101@gmail.com>
2025-02-17 03:16:58 -0700

    slight tmux conf and keymaps.lua changes

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -40,8 +40,7 @@ keymap.set("n", "<leader>j", vim.cmd.bp)
 
 -- window management (close with :q or <leader>x)
 keymap.set("n", "<leader>v", "<C-w>v")             -- split window vertically
-keymap.set("n", "<leader>sh", "<C-w>s")            -- split window horizontally
-keymap.set("n", "<leader>se", "<C-w>=")            -- make split windows equal width & height
+keymap.set("n", "<leader>h", "<C-w>s")             -- split window horizontally
 
 keymap.set("n", "<leader>nt", "<cmd>tabnew<CR>")   -- open new tab
 keymap.set("n", "<leader>n", "<cmd>tabn<CR>")      --  go to next tab
