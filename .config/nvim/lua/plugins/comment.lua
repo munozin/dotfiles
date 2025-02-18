b349ab21a6d32f4ecfe98620f414676d84fa53e8
munozin <cemunoz230101@gmail.com>
2025-02-18 15:30:50 -0700

    added a plugin and git ignore minor changes

--- .config/nvim/lua/plugins/comment.lua
+++
@@ -1,5 +0,0 @@
-return {
-	{
-		"numToStr/Comment.nvim", -- comment line(s) with gc in visual mode and gcc in normal mode
-	}
-}
---
+++ .config/nvim/lua/plugins/some-plugins.lua
@@ -0,0 +1,8 @@
+return {
+	{
+		"christoomey/vim-tmux-navigator" -- Seamless navigation between tmux panes and vim splits
+	},
+	{
+		"numToStr/Comment.nvim" -- comment line(s) with gc in visual mode and gcc in normal mode
+	}
+}
