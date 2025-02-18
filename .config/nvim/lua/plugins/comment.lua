e3456a1b81a817b053b417a621466af5f6111466
munoz <cemunoz230101@gmail.com>
2025-02-18 04:27:44 -0700

    deleted tmux plugin

--- .config/nvim/lua/plugins/comment.lua
+++ .config/nvim/lua/plugins/comment.lua
@@ -1,8 +1,5 @@
 return {
 	{
-		"christoomey/vim-tmux-navigator", -- tmux & split window navigation
-	},
-	{
 		"numToStr/Comment.nvim", -- comment line(s) with gc in visual mode and gcc in normal mode
 	}
 }
